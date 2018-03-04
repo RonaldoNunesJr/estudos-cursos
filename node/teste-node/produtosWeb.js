9278cb81b35841ae35f864eddc7555cbdbcbc832
Ronaldo <ronaldo.nunes.jr@gmail.com>
2018-03-04 20:53:45 -0300

    organizando a pasta

--- node/teste-node/produtosWeb.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-
-http.createServer(function(req, res){
-    res.end('<html><body><h1>Lista de produtos</h1></body></html>')
-}).listen(3000, 'localhost');
-
-console.log("rodando o servidor");
