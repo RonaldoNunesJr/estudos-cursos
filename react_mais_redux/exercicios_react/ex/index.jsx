497bfc8f5dbcefbf8e4f09ad06b02305a34d13f7
Ronaldo Nunes jr <ronaldo.nunes.jr@gmail.com>
2018-03-08 18:57:50 -0300

    inicio do curso de redux

--- react_mais_redux/exercicios_react/ex/index.jsx
+++ react_mais_redux/exercicios_react/ex/index.jsx
@@ -1,7 +1,16 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { combineReducers, createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import Field from "./field"
 
+const reducers = combineReducers({
+    field: () => ({ value: 'Opa' })
+})
+
 ReactDOM.render(
-    <Field value="Teste" initialValue="Escreva Algo" />
+    <Provider store={createStore(reducers)}>
+        <Field value="Teste" initialValue="Escreva Algo" />
+    </Provider>
 , document.getElementById('app'))
