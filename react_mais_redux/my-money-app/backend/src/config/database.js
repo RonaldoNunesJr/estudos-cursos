53421bc91da0917e3be8cc3f71fbe0f3fe58db61
Ronaldo Nunes jr <ronaldo.nunes.jr@gmail.com>
2018-03-28 20:58:17 -0300

    backend finalizado e iniciando criacao do frontend

--- react_mais_redux/my-money-app/backend/src/config/database.js
+++ react_mais_redux/my-money-app/backend/src/config/database.js
@@ -4,5 +4,5 @@ module.exports = mongoose.connect('mongodb://localhost/mymoney')
 
 mongoose.Error.messages.general.required = "0 atributo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = "0 '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
-mongoose.Error.messages.Number.max = "0 '{VALUE}' informado é menor que o limite mínimo de '{MAX}'."
+mongoose.Error.messages.Number.max = "0 '{VALUE}' informado é maior que o limite máximo de '{MAX}'."
 mongoose.Error.messages.String.enum = "'{VALUE}' não é válido para o atributo '{PATH}'."
