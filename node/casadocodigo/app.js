ecfdf9cb267372bf62d648cadac03c183f053685
Ronaldo Nunes jr <ronaldo.nunes.jr@gmail.com>
2018-07-02 13:45:50 -0300

    setup para inicio do curso

--- node/casadocodigo/app.js
+++
@@ -1,9 +0,0 @@
-const app = require('./config/express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-
-app.set('io', io);
-
-http.listen(3000, function(){
-    console.log('servidor em execusão...');
-});
